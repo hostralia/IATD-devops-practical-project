addedd7fe34aa0d24f93d3db356152928f9ad6b4
JM Repos <jawdatjm@gmail.com>
2025-10-07 15:30:41 +1100

    jsut testing

---
+++ practice/firstScript.js
@@ -0,0 +1,11 @@
+import readlineSync from "readline-sync";
+console.log("hello");
+let myName  = "JM";
+console.log(myName);
+let num1= 2
+let num2= 3;
+console.log(num1 + num2);
+readlineSync.question("What is your name? ");
+let reply = readlineSync.question("Enter your reply: ");
+console.log(reply);
+
