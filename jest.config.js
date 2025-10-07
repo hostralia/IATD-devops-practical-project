20316f9d15a537c67c65b8e15d281353a0034a4e
JM Repos <jawdatjm@gmail.com>
2025-10-07 19:29:10 +1100

    added jest config file

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ export default {
  testEnvironment: 'node',
  transform: {}, // required for ESM support
 }
- 
+ /*
+ or do in in the config file :
+
+ reporters: ["default", "github-actions"],
+
+ */
